d61d5a9016bac69a06aba423c1866e52f305f129
Ward <wchamberlain89@gmail.com>
2019-04-19 10:16:19 -0700

    will have sidebar component files

---
+++ src/components/SideNav/CollectionsAdd.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CollectionsAdd() {
+
+}
+
+export default CollectionsAdd;
---
+++ src/components/SideNav/CollectionsList.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CollectionsList() {
+
+}
+
+export default CollectionsList;
