2e127b090c01ed937daf0b92c6bdc3b33bd2712d
Ward <wchamberlain89@gmail.com>
2019-04-19 10:55:19 -0700

    global style will be applied

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import SideNav from './SideNav/SideNav';
 
 import { Switch, Route } from 'react-router-dom';
 
@@ -7,8 +8,18 @@ import Error404 from './Error404';
 function App(){
   return (
     <div>
-
+      <style jsx global>{`
+          * {
+            @import url('https://fonts.googleapis.com/css?family=Nunito:300,400,700');
+            font-family: Nunito;
+            box-sizing : border-box;
+            margin: 0;
+            padding 0;
+          }
+      `}</style>
+      <SideNav/>
       <Switch>
+
         <Route component={Error404}/>
       </Switch>
 
--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function SideNav() {
+    const styles = {
+      width : "15vw",
+      height : "100vh",
+      border : "1px solid black",
+      float : "left"
+    }
 
+    return (
+      <nav style={styles}></nav>
+    )
 }
 
 export default SideNav;
