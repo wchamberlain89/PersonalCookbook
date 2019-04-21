a9ed39d131210ec3237f2a54169f4f16ac628a95
Ward <wchamberlain89@gmail.com>
2019-04-21 12:41:46 -0700

    Welcome page has additional styling

--- src/components/Main/Welcome/Welcome.jsx
+++ src/components/Main/Welcome/Welcome.jsx
@@ -6,20 +6,34 @@ function Welcome () {
   const styles = {
     width: '100%',
     height: '100vh',
+    paddingTop: '25vh',
+    paddingLeft: '10vw',
     display: 'flex',
     flexDirection: 'column',
+
     backgroundImage: 'url(' + backgroundImg + ')',
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
-    backgroundBlendMode: 'multiply',
-    opacity: '0.6'
+
+    position: 'relative'
   };
+
+  const overlay = {
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    right: '0',
+    bottom: '0',
+
+    backgroundColor: 'rgba(0,0,0,0.3)',
+
+  }
   return (
-    <div style={{backgroundColor:"black", width: '100%'}}>
       <div style={styles}>
-        <WelcomeMessage name='ward'/>
+        <WelcomeMessage name='Ward'/>
+        <div style={overlay}>
+        </div>
       </div>
-    </div>
 
   );
 }
--- src/components/Main/Welcome/WelcomeMessage.jsx
+++ src/components/Main/Welcome/WelcomeMessage.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function WelcomeMessage(props) {
+  const styles = {
+    fontSize: '4em',
+    fontWeight: '400',
+    color: '#FFF',
+    zIndex: '10',
+    border: '5px solid white',
+    width: '100%',
+    maxWidth: '30%',
+    minWidth: '200px',
+    padding: '20px'
+  };
+
   return (
-    <h1>
+    <h1 style={styles}>
       Welcome {props.name}
     </h1>
   );
