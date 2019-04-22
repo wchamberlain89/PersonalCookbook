f33613c77c0ddfb247a00b9bbda7037fd6c8144f
Ward <wchamberlain89@gmail.com>
2019-04-21 19:21:05 -0700

    App will be a class based component now

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,31 +7,33 @@ import SideNav from './SideNav/SideNav';
 import Welcome from './Main/Welcome/Welcome';
 import CollectionViewer from './Main/CollectionViewer/CollectionViewer';
 
-function App(){
-  return (
+class App extends React.Component {
+  render() {
+    return (
     <div style={{display: 'flex'}}>
       <style jsx global>
-        {`
-      * {
-        @import url('https://fonts.googleapis.com/css?family=Nunito:300,400,700');
-        font-family: Nunito;
-        box-sizing : border-box;
-        margin: 0;
-        padding: 0;
-        font-size: 14px;
-        font-weight: 300;
-      }
+      {`
+        * {
+          @import url('https://fonts.googleapis.com/css?family=Nunito:300,400,700');
+          font-family: Nunito;
+          box-sizing : border-box;
+          margin: 0;
+          padding: 0;
+          font-size: 14px;
+          font-weight: 300;
+        }
       `}
       </style>
-      <SideNav/>
-      <Switch>
+        <SideNav/>
+        <Switch>
         <Route path="/collections/viewAll" component={CollectionViewer}/>
         <Route exact path='/' component={Welcome}/>
         <Route component={Error404}/>
-      </Switch>
+        </Switch>
 
     </div>
-  );
-}
+      );
+    }
+  }
 
-export default App;
+  export default App;
