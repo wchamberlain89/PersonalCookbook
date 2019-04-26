8a53aa103b1886c2f35b2cfe4954675354655b54
Ward <wchamberlain89@gmail.com>
2019-04-26 13:56:09 -0700

    Took lunch currently researching how to use transitions with react

--- src/components/Main/CollectionDisplay/AddRecipeControl/AddRecipeButton.jsx
+++ src/components/Main/CollectionDisplay/AddRecipeControl/AddRecipeButton.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 function AddRecipeButton() {
   const buttonStyles = {
-    minWidth: '100px',
-    maxWidth: '300px',
+    minWidth: '50px',
     width: '5vw',
 
-    minHeight: '100px',
-    maxHeight: '300px',
+    minHeight: '50px',
     height: '5vw',
 
     position: 'absolute',
@@ -19,8 +17,10 @@ function AddRecipeButton() {
     alignItems: 'center',
 
     borderRadius: '50%',
+    boxShadow: '2px 3px 12px 3px rgba(0,0,0,0.25)',
     backgroundColor: 'red',
 }
+
 return(
   <div style={buttonStyles}>
     <img src={require(`../../../../assets/imgs/add.png`)}/>
